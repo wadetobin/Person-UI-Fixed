92fd8c312d2181c36ee16e8b16ab5f01ce24e315
Wade Tobin <wadetobin@mwc-102186.dhcp.missouri.edu>
2017-09-21 18:37:16 -0500

    First commit

---
+++ Person UI/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Person UI
+//
+//  Created by Wade Tobin on 9/21/17.
+//  Copyright © 2017 Wade Tobin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
